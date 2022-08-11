c13a18fbc9eacd2c70088ac01358806a2fd0e946
lucas <lucashtw@gmail.com>
2022-08-11 16:28:17 -0300

    evoluindo a API modelo MVC

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-import db from "./config/dbConnect.js"
-import livros from "./models/Livro.js"
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("erro",console.log.bind(console,'Erro de conexão'))
 db.once("open", ()=>{
@@ -11,30 +12,11 @@ const app = express();
 
 app.use(express.json())
 
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis"},
-//     {id: 2, "titulo": "O hobbit"}
-// ]
-
-
-app.get('/',(req,res)=>{
-    res.status(200).send('Curso de node');
-})
-
-app.get('/livros', (req,res)=>{
-    livros.find((err, livros)=>{
-        res.status(200).json(livros)
-    })
-})
+routes(app);
 
 app.get('/livros/:id',(req,res)=>{
     let index = buscaLivro(req.params.id);
     res.json(livros[index]);
-})
-
-app.post('/livros',(req,res) =>{
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso')
 })
 
 app.put('/livros/:id',(req,res)=>{
